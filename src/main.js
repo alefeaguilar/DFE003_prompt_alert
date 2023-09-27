5f027cf575431408fec344d17a2446f8cf46b901
alefe aguilar <alefe.wesker@gmail.com>
2023-09-26 21:05:37 -0300

    Questão 2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-//  Elabore um programa que leia um numero e devolva para o usuário o dobro do número informado.
-//const lerNumero = prompt ("Digite um valor");
-//alert (lerNumero * 2);
+// Elabore um programa que leia um numero e devolva para o usuário o dobro do número informado.
+const lerNumero = prompt ("Digite um valor");
+alert (lerNumero * 2);
 
 //Elabore um programa que calcule o valor do jantar, sendo o valor total do jantar composto pela conta junto com a taxa de atendimento do garçom. 
 const valorConta = prompt ("Digite o valor da conta");
